23f7bd69ff40d0449a22fe273e23ef31b6d802f7
Rinit Bhowmick <rbbsl13@gmail.com>
2026-05-05 18:57:24 +0530

    chore: remove legacy HibernateConfiguration and clean up commented legacy mappings in Cart

--- src/main/java/com/jtspringproject/JtSpringProject/models/Cart.java
+++ src/main/java/com/jtspringproject/JtSpringProject/models/Cart.java
@@ -19,38 +19,4 @@ public class Cart {
     @JoinColumn(name="customer_id")
     private User customer;
 
-//    @ManyToMany
-//    @JoinTable(
-//            joinColumns = @JoinColumn(name = "cart_id"),
-//            inverseJoinColumns = @JoinColumn(name = "product_id")
-//    )
-//    private List<Product> products;
-
-
-
-//    public List<Product> getProducts() {
-//        return products;
-//    }
-
-//    public List<Product> getProductsByUser(int customer_id ) {
-//        List<Product> userProducts = new ArrayList<Product>();
-//        for (Product product : products) {
-//            if (product.getCustomer().getId() == customer_id) {
-//                userProducts.add(product);
-//            }
-//        }
-//        return userProducts;
-//    }
-
-//    public void setProducts(List<Product> products) {
-//        this.products = products;
-//    }
-
-//    public void addProduct(Product product) {
-//        products.add(product);
-//    }
-//
-//    public void removeProduct(Product product) {
-//        products.remove(product);
-//    }
 }
